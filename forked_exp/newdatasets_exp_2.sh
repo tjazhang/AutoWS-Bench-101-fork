a7f21944d8c672dea2df0612df7cacf95c3aa979
Jack Zhang <tzhang26@cs.washington.edu>
2023-04-14 12:38:44 -0700

    newdataset\

--- forked_exp/newdatasets_exp_2.sh
+++ forked_exp/newdatasets_exp_2.sh
@@ -7,7 +7,7 @@ export PYTHONPATH="/gscratch/efml/tzhang26/AutoWS-Bench-101-fork"
 
     # python fwrench/applications/pipeline.py --dataset banking77 --lf_selector snuba_multiclass -nlp $lp 
     # python fwrench/applications/pipeline.py --dataset dbpedia --lf_selector snuba_multiclass -nlp $lp 
-python fwrench/applications/pipeline.py --dataset dbpedia-111 --lf_selector snuba_multiclass -nlp $lp 
+python fwrench/applications/pipeline.py --dataset dbpedia-111 --lf_selector snuba_multiclass -nlp 100 
     # python fwrench/applications/pipeline.py --dataset dbpedia-219 --lf_selector snuba_multiclass -nlp $lp 
-python fwrench/applications/pipeline.py --dataset massive-lowcard --lf_selector snuba_multiclass -nlp $lp 
+python fwrench/applications/pipeline.py --dataset massive-lowcard --lf_selector snuba_multiclass -nlp 100 
 
