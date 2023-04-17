4689fcd094a849e431108c71398527a68bdcb5a5
Jack Zhang <tzhang26@cs.washington.edu>
2023-04-17 11:50:10 -0700

    depedia results

--- forked_exp/newdatasets_exp.sh
+++ forked_exp/newdatasets_exp.sh
@@ -6,7 +6,7 @@ export PYTHONPATH="/gscratch/efml/tzhang26/AutoWS-Bench-101-fork"
 
 for lp in 100 300 500;
 do 
-    python fwrench/applications/pipeline.py --dataset banking77 --lf_selector snuba_multiclass -nlp $lp 
+    # python fwrench/applications/pipeline.py --dataset banking77 --lf_selector snuba_multiclass -nlp $lp 
     python fwrench/applications/pipeline.py --dataset dbpedia --lf_selector snuba_multiclass -nlp $lp 
     # python fwrench/applications/pipeline.py --dataset dbpedia-111 --lf_selector snuba_multiclass -nlp $lp 
     python fwrench/applications/pipeline.py --dataset dbpedia-219 --lf_selector snuba_multiclass -nlp $lp 
